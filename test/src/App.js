da41aae72e3e1bb81f79cf97f6864febc79d2515
habibrehman001 <khuwajaabdulrehman046@gmail.com>
2025-12-19 15:32:39 +0500

    Understanding and using states in React

---
+++ test/src/App.js
@@ -0,0 +1,11 @@
+import logo from './logo.svg';
+import './App.css';
+import Textman from './Components/Textman';
+
+function App() {
+  return (
+    <Textman/>
+  );
+}
+
+export default App;
---
+++ test/src/Components/Textman.js
@@ -0,0 +1,48 @@
+import React, { useState } from "react";
+export default function Textman() {
+  const [oldText, newText] = useState("");
+  const chnge = (e) => {
+    newText(e.target.value);
+    console.log("chnge");
+  };
+  const clear = () => {
+    let empty = "";
+    newText(empty);
+    console.log("done");
+  };
+  const upper = () => {
+    let upperTrans = oldText.toUpperCase();
+    newText(upperTrans);
+  };
+  const lower = () => {
+    let upperTrans = oldText.toLowerCase();
+    newText(upperTrans);
+  };
+  return (
+    <>
+      <div className="container">
+        <div className="mb-3">
+          <label htmlFor="exampleFormControlTextarea1" className="form-label">
+            Example textarea
+          </label>
+          <textarea
+            value={oldText}
+            onChange={chnge}
+            className="form-control"
+            id="exampleFormControlTextarea1"
+            rows="3"
+          ></textarea>
+          <button className="btn btn-primary mx-5 my-4" onClick={clear}>
+            Clear Text Area
+          </button>
+          <button className="btn btn-primary mx-5 my-4" onClick={upper}>
+            Upper Case
+          </button>
+          <button className="btn btn-primary mx-5 my-4" onClick={lower}>
+            Lower Case
+          </button>
+        </div>
+      </div>
+    </>
+  );
+}
